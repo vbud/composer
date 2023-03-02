fe66926bb79ef30c758f88ef64f2b4a2902a8326
Valjean Clark <valjeanbud@gmail.com>
2023-03-02 15:04:05 -0800

    ensure Notifications renders in front of the editor

--- src/Notifications/Notifications.css.ts
+++ src/Notifications/Notifications.css.ts
@@ -6,6 +6,7 @@ const distanceFromEdge = 16;
 export const toolbarHidden = style({});
 export const root = style({
   position: 'absolute',
+  zIndex: 1,
   left: '50%',
   transform: `translateX(-50%)`,
   top: toolbarHeight + distanceFromEdge,
